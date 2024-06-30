9724cb9484bc979aaaa71a9fdd3e46f848a2ee8c
ravikannegundla99 <ravikannegundla95@gmail.com>
2024-06-30 17:20:59 -0500

    first commit

---
+++ catalog-service/src/main/java/com/example/bookstore/catalog_service/CatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookstore.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+}
---
+++ catalog-service/src/test/java/com/example/bookstore/catalog_service/TestCatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookstore.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+}
